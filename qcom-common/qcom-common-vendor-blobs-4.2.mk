cd5dc9e1162f7d536169428bc84182b684ad80eb
xylon-reborn <xylon-reborn@outlook.com>
2013-12-18 15:22:17 +0100

    Update qcom-common-4.2 blobs

--- qcom-common/qcom-common-vendor-blobs-4.2.mk
+++ qcom-common/qcom-common-vendor-blobs-4.2.mk
@@ -22,10 +22,10 @@ PRODUCT_COPY_FILES += \
     vendor/sony/qcom-common/proprietary-4.2/etc/firmware/a225p5_pm4.fw:system/etc/firmware/a225p5_pm4.fw \
     vendor/sony/qcom-common/proprietary-4.2/etc/firmware/a300_pfp.fw:system/etc/firmware/a300_pfp.fw \
     vendor/sony/qcom-common/proprietary-4.2/etc/firmware/a300_pm4.fw:system/etc/firmware/a300_pm4.fw \
+    vendor/sony/qcom-common/proprietary-4.2/etc/firmware/a330_pfp.fw:system/etc/firmware/a330_pfp.fw \
+    vendor/sony/qcom-common/proprietary-4.2/etc/firmware/a330_pm4.fw:system/etc/firmware/a330_pm4.fw \
     vendor/sony/qcom-common/proprietary-4.2/etc/firmware/leia_pfp_470.fw:system/etc/firmware/leia_pfp_470.fw \
     vendor/sony/qcom-common/proprietary-4.2/etc/firmware/leia_pm4_470.fw:system/etc/firmware/leia_pm4_470.fw \
-    vendor/sony/qcom-common/proprietary-4.2/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
-    vendor/sony/qcom-common/proprietary-4.2/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
     vendor/sony/qcom-common/proprietary-4.2/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
     vendor/sony/qcom-common/proprietary-4.2/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
     vendor/sony/qcom-common/proprietary-4.2/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
